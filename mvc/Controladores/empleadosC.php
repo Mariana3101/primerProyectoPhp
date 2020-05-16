5f46ec93a6189ad75e4bbd058ca40fdd4f86353f
Mariana3101 <mariana3101@gmail.com>
2020-05-16 17:05:41 -0300

    first commit

---
+++ mvc/Controladores/empleadosC.php
@@ -0,0 +1,46 @@
+<?php
+
+class EmpleadosC
+{
+	//Registrar los empleados
+	public function RegistrarEmpleadosC()
+	{
+		if(isset($_POST["nombreR"])){
+
+			$datosC = array("nombre"=>$_POST["nombreR"],"apellido"=>$_POST["apellidoR"],"email"=>$_POST["emailR"],"puesto"=>$_POST["puestoR"],"salario"=>$_POST["salarioR"]);
+
+			$tablaBD ="empleados";
+
+			$respuesta = EmpleadosM::RegistrarEmpleadosM($datosC, $tablaBD);
+
+			if ($respuesta == "Bien"){
+				header("location:index.php?ruta=empleados");
+			}else{
+				echo "error";
+			}
+		}
+	}
+
+	//Mostrar los empleados
+	public function MostrarEmpleadosC(){
+		$tablaBD = "empleados";
+
+		$respuesta = EmpleadosM::MostrarEmpleadosM($tablaBD);
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+}
+
+
+?>
